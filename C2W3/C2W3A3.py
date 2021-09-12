f82462bc91eaeabadb7f796ba0a840e5a2082eec
Nikita Malovichko <nikita.malovichko@mercaux.com>
2021-09-12 23:00:25 +0200

    c2w3a3 - final

--- C2W3/C2W3A3.py
+++ C2W3/C2W3A3.py
@@ -1,26 +1,38 @@
-from functools import lru_cache
+PRIME_N = 100000004987
+X = 1  # random.randint(1, PRIME_N - 1)
 
 
-@lru_cache()
-def poli_hash(text):
-    prime_n = 250000000007
-    x = 263
-
+def poli_hash(text, x, p):
     r = 0
     for i, s in enumerate(text):
-        r += (ord(s) * x ** i)
-    return r % prime_n
+        r += (ord(s) * x ** i) % p
+    return r % p
+
+
+def precompute_hashes(text, t_len, p_len, x, p):
+    hashed_substrings = [-1 for _ in range(t_len - p_len + 1)]
+    last_substr = text[t_len - p_len:t_len]
+    hashed_substrings[t_len - p_len] = poli_hash(last_substr, x, p)
+
+    y = 1
+    for _ in range(p_len):
+        y = (y * x) % p
+
+    for i in range(t_len - p_len)[::-1]:
+        hashed_substrings[i] = (x * hashed_substrings[i + 1] + ord(text[i]) - y * ord(text[i + p_len])) % p
+    return hashed_substrings
 
 
 def find(pattern, text):
     t_len = len(text)
     p_len = len(pattern)
-    p_hash = poli_hash(pattern)
+    prehash = precompute_hashes(text, t_len, p_len, X, PRIME_N)
+
+    p_hash = poli_hash(pattern, X, PRIME_N)
     for i in range(t_len - p_len + 1):
-        substring = text[i:(i + p_len)]
-        if p_hash != poli_hash(substring):
+        if p_hash != prehash[i]:
             continue
-        if pattern == substring:
+        if pattern == text[i:(i + p_len)]:
             yield i
 
 
@@ -28,5 +40,9 @@ if __name__ == '__main__':
     # print(list(find('aba', 'abacaba')))
     # print(list(find('Test', 'testTesttesT')))
     # print(list(find('aaaaa', 'baaaaaaa')))
+    #
+    # t1 = time.time()
+    # print(list(find('abaab' * 10, 'abaab' * 1000000)))
+    # print(time.time() - t1)
 
     print(' '.join(str(x) for x in find(input(), input())))
